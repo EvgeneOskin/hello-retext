6a1812cc1fdd8d3f40d1dc8a7d332c0181427b9a
EvgeneOskin <eoskin@crystalnix.com>
2017-10-23 01:45:55 +0600

    WIP add sentiment analysis
    Need to create a pretty terminal renderer.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+var fs = require('fs');
+
+var unified = require('unified');
 var retext = require('retext');
 var report = require('vfile-reporter');
+var chalk = require('chalk');
+
+var inspect = require('unist-util-inspect');
 
 var simplify = require('retext-simplify');
 var english = require('retext-english');
@@ -17,6 +23,8 @@ var spell = require('retext-spell');
 var dictionary = require('dictionary-en-us');
 var spacing = require('retext-sentence-spacing');
 
+var text = fs.readFileSync('/dev/stdin').toString()
+
 var processor = retext()
   .use(english)
   .use(spell, dictionary)
@@ -31,11 +39,40 @@ var processor = retext()
   .use(repeated)
   .use(sentiment)
   .use(usage)
-  .use(readability, { age: 16 })  // default age is 16
-  .process(
-  'Go away😠. ' +
-  'You can use cliches until the until the cows come home. ' +
-  'Hey guys, utilize a shorter word.', function (err, file) {
-    console.log(String(file));
-    console.error(report(err || file));
-  });
+  .use(readability, { age: 16 });  // default age is 16
+
+processor.process(
+  text, function (err, file) {
+  console.error(report(err || file));
+});
+
+var sentimentProcessor = unified()
+  .use(english)
+  .use(sentiment);
+
+
+var tree = sentimentProcessor.parse(text);
+
+sentimentProcessor.run(tree, text);
+
+var polarityToColor = new Map()
+polarityToColor.set(-5, chalk.red.underline.bold)
+polarityToColor.set(-4, chalk.red.underline)
+polarityToColor.set(-3, chalk.red.bold)
+polarityToColor.set(-2, chalk.red)
+polarityToColor.set(-1, chalk.red.dim)
+polarityToColor.set(0, chalk.black)
+polarityToColor.set(1, chalk.green.dim)
+polarityToColor.set(2, chalk.green)
+polarityToColor.set(3, chalk.green.bold)
+polarityToColor.set(4, chalk.green.underline)
+polarityToColor.set(5, chalk.green.underline.bold)
+
+debugger
+tree.children.forEach(function (i) {
+  if (i.data && i.data.polarity) {
+    console.log(polarityToColor.get(i.data.polarity)(String(i)));
+  } else {
+    console.log(String(i));
+  }
+})
